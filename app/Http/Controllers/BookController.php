0635b85b3583fe1d655f5383a7254ae11bf57b30
Elnurbek <elnurbek.pubgmobile@gmail.com>
2025-03-03 22:23:44 +0500

    I changed Controller, +data and links

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -1,35 +1,38 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use App\Models\Book;
-use App\Models\Author;
-use Illuminate\Http\Request;
 use App\Http\Resources\BookResource;
-
+use App\Http\Requests\BookSearchRequest;
 class BookController extends Controller
-{
-    
+{  
     public function index()
     {
-        $perPage=request()->per_page ?? 10;
+        $perPage=request()->per_page ?? 1;
         $books=Book::paginate( $perPage);
-        return BookResource::collection($books);
-        
+        return response()->json([
+            'data'=>BookResource::collection($books),
+            'meta'=>[
+                'total'=>$books->total(),
+                'current_page'=>$books->currentPage(),
+                 'per_page'=>$books->perPage(),
+                'last_page'=>$books->lastPage(),
+            ],
+            'links' => [
+                'first' => $books->url(1),
+                'last' => $books->url($books->lastPage()),
+                'prev' => $books->previousPageUrl(),
+                'next' => $books->nextPageUrl(),
+            ]
+        ]);     
     }
-
-    public function search(Request $request)
+    public function search(BookSearchRequest $request)
     {
     $searchTerm = $request->get('query', '');
-    $perPage = $request->get('per_page', 20);
-    $books=Book::paginate( $perPage);
+    $perPage = $request->get('per_page', 2);
     $books = Book::where('title', 'like', "%$searchTerm%")
         ->orWhere('description', 'like', "%$searchTerm%")
         ->orWhere('price', 'like', "%$searchTerm%")
         ->paginate($perPage);
     return response()->json($books);
-    }
-
-    
-    
+    } 
 }
